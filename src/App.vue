d98f93580236d2b68e28ec957c0d07b9378b1118
Sarwan <sarwan.nizamani8@gmail.com>
2020-12-04 02:10:56 -0800

    add components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,49 @@
 <template>
-  <h1>Hello World!</h1>
+  <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
-export default {
 
+import StoredResources from './components/learning-resources/StoredResources.vue'
+export default{
+components:{
+    StoredResources
+},
+data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The Official Vue js Docs',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to Google......',
+          link: 'https://www.google.com',
+        },
+      ],
+    };
+  },
 }
+
 </script>
 
 <style>
 
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
 </style>
---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,23 @@
+<template>
+    <ul>
+    <learning-resource
+      v-for="res in resources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+
+<script>
+import LearningResource from './LearningResource';
+export default {
+    props:['resources'],
+  components: {
+    LearningResource,
+  },
+  
+};
+</script>
